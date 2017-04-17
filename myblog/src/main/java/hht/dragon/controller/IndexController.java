73426937b74366ad218cb4885b53af8318a05e8a
dragon <dragon_hht@163.com>
2017-04-17 21:40:26 +0800

    我的简易博客

--- myblog/src/main/java/hht/dragon/controller/IndexController.java
+++ myblog/src/main/java/hht/dragon/controller/IndexController.java
@@ -43,5 +43,73 @@ public class IndexController {
         return "register";
     }
 
+    @RequestMapping("/index2")
+    public String index2() {
+        return "index_2";
+    }
 
+    @RequestMapping("/home2")
+    public String home2() {
+        return "home_categories_description";
+    }
+
+    @RequestMapping("/home3")
+    public String home3() {
+        return "home_categories_articles";
+    }
+
+    @RequestMapping("/articles")
+    public String articles() {
+        return "articles_list";
+    }
+
+    @RequestMapping("/faq")
+    public String faq() {
+        return "faq";
+    }
+
+    @RequestMapping("/blueSkin")
+    public String blueSkin() {
+        return "blue_skin";
+    }
+
+    @RequestMapping("/greenSkin")
+    public String greenSkin() {
+        return "green_skin";
+    }
+
+    @RequestMapping("/redSkin")
+    public String redSkin() {
+        return "red_skin";
+    }
+
+    @RequestMapping("/fullWidth")
+    public String fullWidth() {
+        return "full_width";
+    }
+
+    @RequestMapping("/elements")
+    public String elements() {
+        return "elements";
+    }
+
+    @RequestMapping("/page")
+    public String page() {
+        return "page";
+    }
+
+    @RequestMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @RequestMapping("/single")
+    public String single() {
+        return "single";
+    }
+
+    @RequestMapping("/maintop")
+    public String maintop() {
+        return "main_top";
+    }
 }
